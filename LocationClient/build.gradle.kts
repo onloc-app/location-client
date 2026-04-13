3cbeb4e2843540d330cd1fbb7ddcf124a825d04e
calvicii <lavoiethomas17@gmail.com>
2026-04-13 15:25:08 -0400

    Add publishing to Maven Central

--- LocationClient/build.gradle.kts
+++ LocationClient/build.gradle.kts
@@ -1,7 +1,24 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
+
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.maven.publish)
+    id("signing")
 }
+
+val properties = Properties().apply {
+    val file = rootProject.file("local.properties")
+    if (file.exists()) {
+        load(file.inputStream())
+    }
+}
+
+group = "app.onloc"
+version = "1.0.0"
 
 android {
     namespace = "app.onloc.locationclient"
@@ -26,13 +43,52 @@ android {
     }
     kotlin {
         compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+}
+
+mavenPublishing {
+    configure(
+        AndroidSingleVariantLibrary(
+            variant = "release",
+            sourcesJar = true,
+            publishJavadocJar = true,
+        )
+    )
+
+    publishToMavenCentral()
+    signAllPublications()
+
+    coordinates("app.onloc", "location-client", "1.0.0")
+
+    pom {
+        name.set("Location Client")
+        description.set("Simple location client for Android. Returns device's location using GPS and network while removing the hassle of dealing with LocationManager.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/onloc-app/location-client")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("Calvicii")
+                name.set("Thomas Lavoie")
+                url.set("https://github.com/calvicii")
+            }
+        }
+        scm {
+            url.set("https://github.com/onloc-app/location-client")
+            connection.set("scm:git:git://github.com/onloc-app/location-client.git")
         }
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
